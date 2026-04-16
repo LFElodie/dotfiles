2011b34d83b0f138a51b3d3469269b947203af29
LFElodie <lanfei419@gmail.com>
2026-04-16 13:24:23 +0800

    feat(nvim): make telescope file search backend-aware

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,54 @@
 -- plugins/telescope.lua
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-lua/popup.nvim",
     "nvim-telescope/telescope-fzy-native.nvim",
   },
   config = function()
-    require("telescope").setup({
+    local ok_ts_parsers, ts_parsers = pcall(require, "nvim-treesitter.parsers")
+    if ok_ts_parsers and ts_parsers.ft_to_lang == nil and vim.treesitter.language.get_lang ~= nil then
+      ts_parsers.ft_to_lang = function(ft)
+        return vim.treesitter.language.get_lang(ft) or ft
+      end
+    end
+
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+
+    local function get_find_command()
+      if vim.fn.executable("rg") == 1 then
+        return { "rg", "--files", "--color", "never" }
+      end
+      if vim.fn.executable("fd") == 1 then
+        return { "fd", "--type", "f", "--color", "never" }
+      end
+      if vim.fn.executable("fdfind") == 1 then
+        return { "fdfind", "--type", "f", "--color", "never" }
+      end
+      if vim.fn.executable("find") == 1 then
+        return { "find", ".", "-type", "f" }
+      end
+      return nil
+    end
+
+    local function find_project_files(opts)
+      opts = opts or {}
+      local find_command = get_find_command()
+      if not find_command then
+        vim.notify(
+          "Telescope find_files needs one of: rg, fd, fdfind, find",
+          vim.log.levels.ERROR
+        )
+        return
+      end
+
+      opts.find_command = find_command
+      builtin.find_files(opts)
+    end
+
+    telescope.setup({
       defaults = {
         file_sorter = require("telescope.sorters").get_fzy_sorter,
         color_devicons = true,
@@ -19,8 +59,8 @@ return {
           "build",
           "dotbot",
           "pycache",
-          "lyenv",
-          "zsh"
+          "dev_venv",
+          "zsh",
         },
         mappings = {
           i = {
@@ -30,49 +70,14 @@ return {
         },
       },
     })
-    require("telescope").load_extension("fzy_native")
+    pcall(telescope.load_extension, "fzy_native")
 
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>ss",
-      [[<cmd>lua require('telescope.builtin').file_browser()<CR>]],
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>sb",
-      [[<cmd>lua require('telescope.builtin').buffers()<CR>]],
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>sf",
-      [[<cmd>lua require('telescope.builtin').find_files()<CR>]],
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>sh",
-      [[<cmd>lua require('telescope.builtin').help_tags()<CR>]],
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>sd",
-      [[<cmd>lua require('telescope.builtin').grep_string()<CR>]],
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>sg",
-      [[<cmd>lua require('telescope.builtin').live_grep()<CR>]],
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<leader>/",
-      [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]],
-      { noremap = true, silent = true }
-    )
+    vim.keymap.set("n", "<leader>ss", find_project_files, { silent = true })
+    vim.keymap.set("n", "<leader>sb", builtin.buffers, { silent = true })
+    vim.keymap.set("n", "<leader>sf", find_project_files, { silent = true })
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { silent = true })
+    vim.keymap.set("n", "<leader>sd", builtin.grep_string, { silent = true })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { silent = true })
+    vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { silent = true })
   end,
 }
