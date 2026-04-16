6f140b75fa910bef20913fb82d0527bead5e784a
LFElodie <lanfei419@gmail.com>
2026-04-16 13:21:36 +0800

    chore(install): align apt bootstrap with python tooling

--- install_packages.sh
+++ install_packages.sh
@@ -1,11 +1,15 @@
-if  [ -x "$(command -v brew)" ]; then
-  echo 'install package using brew'
-  brew install zsh neovim autojump tmux ranger fzf
+#!/usr/bin/env bash
+
+set -e
+
+if  [ -x "$(command -v apt-get)" ]; then
+  echo 'install package using apt'
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update
+  sudo apt-get install -y \
+    zsh neovim autojump tmux ranger fzf curl git cmake \
+    python3 python3-pip python3-venv python3-dev \
+    clang-format ripgrep fd-find xclip nodejs npm terminator
   exit 0
 fi
-if  [ -x "$(command -v brew)" ]; then
-  echo 'install package using brew'
-  sudo apt install zsh neovim autojump tmux ranger fzf
-  exit 0
-fi
-echo 'brew and apt not find'
+echo 'apt not find'
--- install_scripts/setup_packages.sh
+++ install_scripts/setup_packages.sh
@@ -4,9 +4,9 @@ set -e
 # 安装常用软件包
 sudo apt-get update
 sudo apt-get install -y \
-    zsh autojump tmux ranger curl git cmake python3-pip fzf ccls \
+    zsh autojump tmux ranger curl git cmake python3 python3-pip python3-venv python3-dev fzf ccls \
     clang-format ripgrep fd-find htop terminator xclip nodejs npm \
-    python3-venv gnome-tweaks
+    gnome-tweaks
 
 # 添加 neovim-ppa 源并安装 nightly 版 neovim
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
@@ -14,7 +14,7 @@ sudo apt-get update
 sudo apt-get install -y neovim
 
 # pip 安装 neovim 支持
-pip3 install --user neovim
+python3 -m pip install --user --upgrade pip setuptools wheel pynvim
 
 # 使用 npm 安装最新稳定版 node
 sudo npm install -g n
